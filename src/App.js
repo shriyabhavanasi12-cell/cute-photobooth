ea0c753cc98f97f5a24ca0f2a464deedbd4edef6
shriyabhavanasi12-cell <shriyabhavanasi.12@gmail.com>
2026-09-17 17:49:11 +0530

    add Navbar

--- src/App.js
+++ src/App.js
@@ -8,21 +8,37 @@ const logoSrc="/assets/logo/jiggleduo-logo.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{
+      minHeight:"100vh",
+      display:"flex",
+      flexDirection:"column",
+      alignItems:"center"
+    }}>
+
+<div style={{
+  width: "100%",
+  maxWidth:1200,
+  display:"flex",
+alignItems:"center",
+gap:12,
+padding:"20px 32px"
+
+}}>
+
+
+
+<img src={logoSrc} alt="JiggleDuo Logo" style={{width: 50}}/>
+
+<h1 style={{
+  fontFamily:"Cantika Cute",
+  color:"#8c5b4a",
+  margin:0
+}}>
+JiggleDuo Photobooth 
+
+  
+</h1>
+      </div>
     </div>
   );
 }
