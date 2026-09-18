bcdde8cd815d96d9008e98c707629610c2205f0a
shriyabhavanasi12-cell <shriyabhavanasi.12@gmail.com>
2026-09-18 18:56:58 +0530

    add  webcam configuration and frame selection

---
+++ src/components/photobooth.js
@@ -0,0 +1,33 @@
+import React,{useRef,useActionState,useEffect} from "react";
+import Webcam from "react-webcam";
+
+const frameOptions=[
+    "/assets/frames/heart-frame.png",
+    "/assets/frames/heart-frame-2.png",
+    "/assets/frames/heart-frame-3.png",
+"/assets/frames/heart-frame-4.png",
+
+]
+
+const stickersOptions=[
+    "/assets/stickers/leaf.png",
+    "/assets/stickers/sparkles.png"
+];
+
+const videoConstraints={width: 953,height:599,facingMode:"user"};
+const SLOT_WIDTH=953;   
+const SLOT_HEIGHT=599;
+
+export default function Photobooth(){
+    const webcamRef=useRef(null);
+    const canvasRef=useRef(null);
+    const frameImgRef=useRef(null);
+
+    const slots=[
+        {x: 123,y:78},
+        {x:123,y:697},
+        {x: 123, y:1286},
+        {x: 123, y:1885},
+    ]
+    
+}
